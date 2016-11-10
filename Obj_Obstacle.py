207ed8a2787b9604fbee93f7cecb5b26ab080a23
Kkoding <pkgore@naver.com>
2016-11-10 20:51:25 +0900

    UI, UX, Be Attacked Pattern

---
+++ Obj_Obstacle.py
@@ -0,0 +1,19 @@
+from pico2d import *
+
+import game_framework
+
+import Main
+
+
+class Obstacle:
+	def __init__(self):
+		#meteo 떨어진다.
+		self.warning  = load_image ('D:\\2-2\\2DGP\\object\\warn_line.png')
+		self.warning_mark = load_image ('D:\\2-2\\2DGP\\object\\warn_makr.png')
+		self.meteo = load_image ('D:\\2-2\\2DGP\\object\\meteo.png')
+		
+		
+	def draw_meteo(self):
+		self.warning.draw_to_origin(150,150,32,256)
+	
+	
